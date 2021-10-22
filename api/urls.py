c39b45c1368fca62dea4e02210555d8aa0f231f7
fedotovdmitriy14 <fedotovdmitriy14@gmail>
2021-10-23 00:49:07 +0300

    add join room functionality

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.RoomView.as_view(), name='room_view'),
-    path('create-room', views.CreateRoomView.as_view())
+    path('create-room', views.CreateRoomView.as_view()),
+    path('get-room', views.GetRoom.as_view()),
+    path('join-room', views.JoinRoom.as_view())
 ]
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CreateRoomPage from "./CreateRoomPage"
 import RoomJoinPage from "./RoomJoinPage"
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
+import Room from './Room'
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -14,6 +15,7 @@ export default class HomePage extends Component {
                 <Route exact path='/'><p>This is the home page</p></Route>    // exact!!!!!!!!!! (иначе будут одинаковые адреса)
                 <Route path='/join' component={RoomJoinPage} />
                 <Route path='/create-room' component={CreateRoomPage} />
+                <Route path='/room/:roomCode' component={Room} />
                </Switch>
         </Router>
     }
