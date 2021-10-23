70d3c5a82c438eff26de2ef7fc267ddc6220c633
fedotovdmitriy14 <fedotovdmitriy14@gmail>
2021-10-23 22:14:55 +0300

    add leave room functionality

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.RoomView.as_view(), name='room_view'),
     path('create-room', views.CreateRoomView.as_view()),
     path('get-room', views.GetRoom.as_view()),
-    path('join-room', views.JoinRoom.as_view())
+    path('join-room', views.JoinRoom.as_view()),
+    path('user-in-room', views.UserInRoom.as_view()),
+    path('leave-room', views.LeaveRoom.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -77,3 +77,25 @@ class JoinRoom(APIView):
 
         return Response({'Bad request': 'Invalid post data'}, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')        # remove info from session
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
