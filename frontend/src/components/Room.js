6be94500a79f8b8eb7f4907a376bc580f91925cc
fedotovdmitriy14 <fedotovdmitriy14@gmail>
2021-10-24 17:26:59 +0300

    add update room functionality

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import CreateRoomPage from './CreateRoomPage'
-import { Grid, Button, Typography } from '@material-ui/core'
-
+import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class Room extends Component {
   constructor(props) {
@@ -13,68 +12,15 @@ export default class Room extends Component {
       showSettings: false,
     };
     this.roomCode = this.props.match.params.roomCode;
-    this.getRoomDetails();
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
     this.updateShowSettings = this.updateShowSettings.bind(this);
     this.renderSettingsButton = this.renderSettingsButton.bind(this);
     this.renderSettings = this.renderSettings.bind(this);
+    this.getRoomDetails = this.getRoomDetails.bind(this);
+    this.getRoomDetails();
   }
 
-
-   leaveButtonPressed() {
-    const requestOptions = {
-      method: "POST",
-      headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-      },
-    };
-    fetch("/api/leave-room", requestOptions).then((_response) => {
-      this.props.leaveRoomCallback();
-      this.props.history.push("/");
-    });
-  }
-
-
-  updateShowSettings(value) {
-    this.setState({
-        showSettings: value,
-    })
-  }
-
-  renderSettings() {
-    return (<Grid container spacing={1}>
-        <Grid item xs={12} align='center'>
-            <CreateRoomPage update={true}
-            votesToSkip={this.state.votesTosSkip}
-            guestCanPause={this.state.guestCanPause}
-            roomCode={this.roomCode}
-            updateCallback={() => {}}
-            />
-        </Grid>
-        <Grid item xs={12} align='center'>
-            <Button
-                variant='contained'
-                color='secondary'
-                onClick={() => this.updateShowSettings(false)}>
-                Close
-            </Button>
-        </Grid>
-    </Grid>
-    )
-  }
-
-  renderSettingsButton() {
-    return (
-        <Grid item xs={12} align='center'>
-            <Button variant='contained' color='primary' onClick={() => this.updateShowSettings(true)}>
-                Settings
-            </Button>
-        </Grid>
-    )
-  }
-
-    getRoomDetails() {
+  getRoomDetails() {
     return fetch("/api/get-room" + "?code=" + this.roomCode)
       .then((response) => {
         if (!response.ok) {
@@ -92,13 +38,68 @@ export default class Room extends Component {
       });
   }
 
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push("/");
+    });
+  }
+
+  updateShowSettings(value) {
+    this.setState({
+      showSettings: value,
+    });
+  }
+
+  renderSettings() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <CreateRoomPage
+            update={true}
+            votesToSkip={this.state.votesToSkip}
+            guestCanPause={this.state.guestCanPause}
+            roomCode={this.roomCode}
+            updateCallback={this.getRoomDetails}
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.updateShowSettings(false)}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  }
+
   render() {
     if (this.state.showSettings) {
-        return this.renderSettings()
+      return this.renderSettings();
     }
-
     return (
-         <Grid container spacing={1}>
+      <Grid container spacing={1}>
         <Grid item xs={12} align="center">
           <Typography variant="h4" component="h4">
             Code: {this.roomCode}
